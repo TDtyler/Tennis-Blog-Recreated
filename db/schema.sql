2637f7899eddc2c01caeba7eae34da8373288af6
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-05-20 09:21:50 -0700

    created database

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS prince_of_tennis_db;
+
+CREATE DATABASE prince_of_tennis_db;
+
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 const PORT = 3001;
 const app = express();
 
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log("GoodBye");
+//connect to the database & then run the server
+sequelize.sync({}).then(() => {
+    app.listen(PORT, () => {
+        console.log("GoodBye");
+})
+
 })
