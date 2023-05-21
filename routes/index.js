c764187a4791167d3c86cdac543c8f028d9d3cbd
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-05-20 19:34:21 -0700

    added process.env.port to deploy

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const users = require('./users');
+const pages = require('./pages');
 
 // localhost:3001/
-
+router.use('/', pages);
 router.use("/users", users);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const PORT = 3001;
+const exphbs = require('express-handlebars');
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+// to access the public content
+app.use(express.static('public'));
+
+// Handlebars
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set("view engine", "handlebars");
 
 // this is needed for post request
 app.use(express.json());
