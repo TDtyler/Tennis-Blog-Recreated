8d2ea8867293c8de1effdc558d63a78df5880f9e
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-05-21 09:44:52 -0700

    made a dashboard

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const { User} = require("../../models");
+
 
 router.get('/', (req, res) => {
     res.render('homepage');
 })
 
+router.get('/dashboard/:user_id', async (req, res) => {
+    const userData = await User.findByPk(req.params.user_id, {
+        attributes: [
+            'username',
+            'email'
+        ]
+    });
+
+    const actualUserData = userData.get({plain: true});
+    res.render('dashboard', actualUserData);
+})
 module.exports = router;
